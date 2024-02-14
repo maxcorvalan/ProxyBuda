57c9a78120d0a34061d3fa9c112bd49ee51061ea
maxcorvalan <53880732+maxcorvalan@users.noreply.github.com>
2024-02-14 10:05:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://dca-buda.onrender.com' // Reemplaza esto con el origen de tu frontend
+}));
+
 app.use(express.json());
 
 app.get('/api/trades', async (req, res) => {
